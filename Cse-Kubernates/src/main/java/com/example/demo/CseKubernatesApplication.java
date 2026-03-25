3e1406144ac1862838d79a71ba655f11d5386c12
Harini-Upparapalli <24p35a0555@acet.ac.in>
2026-03-25 10:33:36 +0530

    initial commit

---
+++ Cse-Kubernates/src/main/java/com/example/demo/CseKubernatesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CseKubernatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CseKubernatesApplication.class, args);
+	}
+
+}
